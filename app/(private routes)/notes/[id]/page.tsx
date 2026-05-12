ba64346464e849a21cf9b37f28cb186d4a578a18
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-05-12 13:46:29 +0300

    relocated folder notes

---
+++ app/(private routes)/notes/[id]/page.tsx
@@ -0,0 +1,53 @@
+
+import { getServerSingleNote } from "@/lib/api/serverApi";
+import NoteDetailsClient from "./NoteDetails.client";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const note = await getServerSingleNote(id);
+
+  return {
+    title: `Note: ${note.title}`,
+    description: note.content.slice(0, 30),
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description: note.content.slice(0, 30),
+      url: `/notes/${id}`,
+      images: [
+        {
+          url: "/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "NoteHub preview image",
+        },
+      ],
+    },
+  };
+}
+
+export default async function NotesPage({ params }: Props) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => getServerSingleNote(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
+}
