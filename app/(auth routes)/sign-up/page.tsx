f7c003a813b18653d390f3b2a96bddd7ad0c42cd
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-05-12 13:20:06 +0300

    fixed User's type

--- app/(auth routes)/sign-up/page.tsx
+++ app/(auth routes)/sign-up/page.tsx
@@ -35,10 +35,6 @@ export default function SignUp() {
     <main className={css.mainContent}>
   <h1 className={css.formTitle}>Sign up</h1>
         <form className={css.form} action={handleSubmit}>
-           <div className={css.formGroup}>
-      <label htmlFor="username">Username</label>
-      <input type="text" name="userName" className={css.input} required />
-    </div>
     <div className={css.formGroup}>
             <label htmlFor="email">Email</label>
             <input id="email" type="email" name="email" className={css.input} required />
